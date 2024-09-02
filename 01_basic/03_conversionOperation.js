ac95b7ba5ce686513fd02b659628e069b246de07
somaditya tripathi <115528498+SOMADITYAT@users.noreply.github.com>
2024-09-02 21:50:11 +0000

    Conversion of datatype

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "Somaditya"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score)
+
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "23" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+let isLogginedIn = "Somaditya";
+
+let booleanIsloading = Boolean(isLogginedIn)
+// console.log(booleanIsloading);
+
+//  1 => true; 0 => false 
+//  "" => false 
+//  "Sommaditya" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber);
+
+console.log(someNumber);
+console.log(typeof someNumber);
+
+
+
+
