5eba62288b6b1abed5e49c7bb7104552afc39a58
somaditya tripathi <115528498+SOMADITYAT@users.noreply.github.com>
2024-09-02 21:30:05 +0000

    datatypes

---
+++ 01_basic/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict" ; // treat all Js code as newer version
+
+// alert(9+9) // we are using node.js , not broswers
+
+console.log(3+3)   // 
+
+console.log("Somaditya");
+
+
+let name = "Somaditya"
+let age = "24"
+let isLogginedIn = false 
+
+
+// number => to power 53
+//bigint  =>
+//string => ""
+//boolean => true/false
+//null => standalone value
+// undefined => 
+// symbol =>  unique     
+
+// object 
+
+console.log(typeof null); // object
+console.log(typeof undefined);// undefined
+
+
