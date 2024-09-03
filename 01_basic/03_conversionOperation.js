d5eb2d71af27c99200b3cd7c2ec2f5ca092984e7
somaditya tripathi <115528498+SOMADITYAT@users.noreply.github.com>
2024-09-03 00:19:27 +0000

    datatype summary

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,9 +1,9 @@
-let score = "Somaditya"
+let score = "Somaditya";
 
 // console.log(typeof score);
 // console.log(typeof (score));
 
-let valueInNumber = Number(score)
+let valueInNumber = Number(score);
 
 // console.log(typeof valueInNumber);
 // console.log( valueInNumber);
@@ -12,23 +12,48 @@ let valueInNumber = Number(score)
 // "33abc" => NaN
 // true => 1; false => 0
 
-
 let isLogginedIn = "Somaditya";
 
-let booleanIsloading = Boolean(isLogginedIn)
+let booleanIsloading = Boolean(isLogginedIn);
 // console.log(booleanIsloading);
 
-//  1 => true; 0 => false 
-//  "" => false 
+//  1 => true; 0 => false
+//  "" => false
 //  "Sommaditya" => true
 
-let someNumber = 33
+let someNumber = 33;
 
 let stringNumber = String(someNumber);
 
-console.log(someNumber);
-console.log(typeof someNumber);
+// console.log(someNumber);
+// console.log(typeof someNumber);
 
+// *********************** Operations ***********************
 
+let value = 3;
+let negValue = -value;
+// console.log(negValue);
 
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
 
+let str1 = "Hello";
+let str2 = " Somaditya";
+let str3 = str1 + str2;
+console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+"");
+
+//  link to study
+
+// https://tc39.es/ecma262/2023/multipage/abstract-operations.html#sec-type-conversion
